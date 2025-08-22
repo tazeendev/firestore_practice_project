1b05637dc0a6b5f6b4d335b5c9d8a3883456b26f
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-08-22 07:57:25 -0700

    .

---
+++ lib/sub-collections/sub_collection_2/sub_collection_2.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+class SubCollectionFetchData extends StatefulWidget {
+  const SubCollectionFetchData({super.key});
+
+  @override
+  State<SubCollectionFetchData> createState() => _SubCollectionFetchDataState();
+}
+
+class _SubCollectionFetchDataState extends State<SubCollectionFetchData> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+//-------------------insertSubData------------------
+class InsertSubData extends StatefulWidget {
+  const InsertSubData({super.key});
+
+  @override
+  State<InsertSubData> createState() => _InsertSubDataState();
+}
+
+class _InsertSubDataState extends State<InsertSubData> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
+
